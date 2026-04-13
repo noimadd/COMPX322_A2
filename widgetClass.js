93b7a1f0f2329663cb04ba3f08dbc69d51e02705
ds526-student <damionsklenarsclare@gmail.com>
2026-04-14 01:57:56 +1200

    access api
    accessing api based on widget selection and outputting data to the console

--- widgetClass.js
+++ widgetClass.js
@@ -1,5 +1,5 @@
 
-const API_KEY = 'null';
+const API_KEY = '';
 const API_URL = 'https://api.api-ninjas.com/v1/population';
 
 const DATA_OPTIONS = {
@@ -83,16 +83,57 @@ class Widget {
 
         this.titleElement = this.element.querySelector('.widget-title');
         this.chartElement = this.element.querySelector('.widget-chart');
-        this.dataSelect = this.element.querySelector('.data-select');
-        this.countrySelect = this.element.querySelector('.country-select');
-        this.chartSelect = this.element.querySelector('.chart-select');
+        this.dataSelect = this.element.querySelector('.data-select select');
+        this.countrySelect = this.element.querySelector('.country-select select');
+        this.chartSelect = this.element.querySelector('.chart-select select');
 
         this.container.appendChild(this.element);
     }
 
     attachEventListeners() {
+        this.dataSelect.addEventListener('change', () => this.fetchData());
+        this.countrySelect.addEventListener('change', () => this.fetchData());
+
+        this.chartSelect.addEventListener('change', () => { if (this.apiData) this.updateChart(); });
+
         this.element.querySelector('.widget-close-btn').addEventListener('click', () => {
             this.element.remove();
         });
+    }
+
+    async fetchData() {
+        const COUNTRY = this.countrySelect.value;
+        const dataLabel = this.dataSelect.value;
+
+        console.log(`Fetching data for ${COUNTRY} - ${dataLabel}`);
+
+        if (!COUNTRY || !dataLabel) return;
+
+        try {
+            const response = await fetch(
+                `${API_URL}?country=${encodeURIComponent(COUNTRY)}`,
+                { headers:
+                    { 'X-Api-Key': API_KEY }
+                }
+            )
+
+            if (!response.ok) { throw new Error(`API error: ${response.status}`); }
+
+            const data = await response.json();
+
+            const historical = data.historical_population;
+            const forecast = data.population_forecast;
+
+            this.apiData = {
+                historical: historical.map(entry => ({ year: entry.year, value: entry[DATA_OPTIONS[dataLabel]] })),
+                forecast: forecast.map(entry => ({ year: entry.year, value: entry[DATA_OPTIONS[dataLabel]] }))
+            };
+
+            console.log(this.apiData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            alert('Failed to fetch data. Please check the console for details.');
+            return;
+        }
     }
 }
